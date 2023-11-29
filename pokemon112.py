d2e053ed389f1393f86dde90f6d8ecad8983d7be
MartinJ45 <bolitaycamilo45@gmail.com>
2023-11-29 09:35:43 -0500

    Added more in battle and overworld sprites; Added item functionality

--- pokemon112.py
+++ pokemon112.py
@@ -2,96 +2,39 @@ from cmu_graphics import *
 from PIL import Image
 import os, pathlib
 from random import *
+import numpy as np
 from pokemon112_classes import *
-
-def test():
-    # this is a test function that has no purpose in the actual game
-    pokemon1 = Pokemon('Bulbasaur', 5)
-    pokemon2 = Pokemon('Charmander', 5)
-    pokemon1.updateMoves()
-    pokemon2.updateMoves()
-    print(pokemon1)
-    print(pokemon2)
-
-    print(pokemon1.attackPokemon(pokemon2, pokemon1.moves[0]))
-
-    print(pokemon1)
-    print(pokemon2)
-
-    # route1grid = [
-    #     [1] * 7 + [1] * 8 + [0] * 4 + [1] * 8 + [1] * 7,
-    #     [1] * 7 + [1] * 8 + [0] * 2 + [100] + [0] + [1] * 8 + [1] * 7,
-    #     [1] * 7 + [1] * 8 + [0] * 4 + [1] * 8 + [1] * 7,
-    #     [1] * 7 + [1] * 8 + [0] * 4 + [1] * 8 + [1] * 7,
-    #     [1] * 7 + [1] * 8 + [0] * 4 + [1] * 8 + [1] * 7,
-    #     [1] * 7 + [1] * 8 + [0] * 4 + [1] * 8 + [1] * 7,
-    #     [1] * 7 + [0] * 20 + [1] * 7,
-    #     [1] * 7 + [0] * 20 + [1] * 7,
-    #     [1] * 7 + [0] * 6 + [1] * 2 + [0] * 12 + [1] * 7,
-    #     [1] * 7 + [2] * 6 + [1] * 2 + [2] * 6 + [0] * 6 + [1] * 7,
-    #     [1] * 7 + [0] * 6 + [1] * 2 + [3] * 12 + [1] * 7,
-    #     [1] * 7 + [0] * 6 + [1] * 2 + [3] * 12 + [1] * 7,
-    #     [1] * 7 + [0] * 6 + [1] * 2 + [3] * 12 + [1] * 7,
-    #     [1] * 7 + [0] * 6 + [1] * 2 + [3] * 12 + [1] * 7,
-    #     [1] * 7 + [2] * 6 + [1] * 2 + [3] * 12 + [1] * 7,
-    #     [1] * 7 + [0] * 6 + [1] * 2 + [0] * 12 + [1] * 7,
-    #     [1] * 7 + [0] * 20 + [1] * 7,
-    #     [1] * 7 + [0] * 14 + [3] * 6 + [1] * 7,
-    #     [1] * 7 + [0] * 14 + [3] * 6 + [1] * 7,
-    #     [1] * 7 + [1] * 2 + [2] * 6 + [1] * 6 + [3] * 6 + [1] * 7,
-    #     [1] * 7 + [1] * 2 + [0] * 6 + [1] * 6 + [3] * 6 + [1] * 7,
-    #     [1] * 7 + [0] * 14 + [3] * 6 + [1] * 7,
-    #     [1] * 7 + [0] * 20 + [1] * 7,
-    #     [1] * 7 + [0] * 20 + [1] * 7,
-    #     [1] * 7 + [2] * 2 + [0] * 1 + [2] * 4 + [0] * 2 + [2] * 11 + [1] * 7,
-    #     [1] * 7 + [0] * 20 + [1] * 7,
-    #     [1] * 7 + [0] * 20 + [1] * 7,
-    #     [1] * 7 + [0] * 20 + [1] * 7,
-    #     [1] * 7 + [0] * 10 + [3] * 6 + [0] * 4 + [1] * 7,
-    #     [1] * 7 + [1] * 10 + [3] * 6 + [0] * 4 + [1] * 7,
-    #     [1] * 7 + [1] * 10 + [3] * 6 + [2] * 4 + [1] * 7,
-    #     [1] * 7 + [0] * 10 + [3] * 6 + [0] * 4 + [1] * 7,
-    #     [1] * 7 + [0] * 10 + [3] * 6 + [0] * 4 + [1] * 7,
-    #     [1] * 7 + [0] * 20 + [1] * 7,
-    #     [1] * 7 + [0] * 20 + [1] * 7,
-    #     [1] * 7 + [2] * 4 + [0] * 3 + [1] * 1 + [2] * 12 + [1] * 7,
-    #     [1] * 7 + [0] * 2 + [3] * 7 + [0] * 6 + [3] * 5 + [1] * 7,
-    #     [1] * 7 + [0] * 2 + [3] * 7 + [0] * 6 + [3] * 5 + [1] * 7,
-    #     [1] * 7 + [3] * 7 + [0] * 6 + [3] * 5 + [0] * 2 + [1] * 7,
-    #     [1] * 7 + [3] * 7 + [0] * 3 + [3] * 2 + [0] * 1 + [3] * 5 + [0] * 2 + [1] * 7,
-    #     [1] * 7 + [1] * 10 + [3] * 2 + [1] * 8 + [1] * 7,
-    #     [1] * 7 + [1] * 10 + [3] * 2 + [1] * 8 + [1] * 7,
-    #     [1] * 7 + [1] * 10 + [3] * 2 + [1] * 8 + [1] * 7,
-    #     [1] * 7 + [1] * 10 + [3] * 2 + [1] * 8 + [1] * 7,
-    # ]
-    #
-    # for r in route1grid:
-    #     print(r)
 
 def openImage(fileName):
     # opens up sprites (currently not in use since graphics will be added later)
     return Image.open(os.path.join(pathlib.Path(__file__).parent,fileName))
 
 def onAppStart(app):
-    app.stepsPerSecond = 30
+    app.stepsPerSecond = 15
     app.counter = 0
 
-    app.scenes = ['overworld', 'battle']
+    app.scenes = ['overworld', 'battle', 'menu']
     # overworld - where the player can walk around and interact with trainers and items
     # battle - where the player battles either wild pokemon or other trainers
     # buildings - where the player can walk around and interact with the world, but in a building (different map)
     app.scene = 0
+    app.menuScreens = ['pokemon', 'item']
+    app.menuScreenIndex = 1
 
     app.cellWidth = app.cellHeight = 32
     app.mapGrid = loadMapGrid()
+    loadOverworldGraphics(app)
     app.overworld = setupOverworld(app)
     app.cameraPos = [5, 5]
     app.cameraPosD = [0, 0]
     app.cellD = [32, 32]
     app.playerPos = [app.cameraPos[0] + 5, app.cameraPos[1] + 7]
 
-    # temporary setup for trainer and one pokemon
-    app.player = Trainer('Player')
+    # setup for trainer
+    playerSpriteSheet = getPlayerSprites()
+    app.player = Trainer('Player', playerSpriteSheet)
+    app.player.addItem('Potion')
+    app.player.addItem('Potion')
     pokemon1 = Pokemon('Bulbasaur', 5)
     pokemon1.updateStats()
     pokemon1.updateMoves()
@@ -100,14 +43,15 @@ def onAppStart(app):
     app.curPokemon = app.player.party[0]
 
     # temporary setup for opponent and one pokemon
-    app.opponent = Trainer('Opponent')
+    app.opponent = Trainer('Opponent', playerSpriteSheet, 18, 12)
+    app.opponent.facing = 'left'
     pokemon2 = Pokemon('Charmander', 5)
     pokemon2.updateStats()
     pokemon2.updateMoves()
     app.opponent.addToParty(pokemon2)
-    # app.oppPokemon will keep track of the current pokemon that the opponent would have out in a battle
-    app.oppPokemon = app.opponent.party[0]
+    app.wildBattle = True
 
+    loadMenuGraphics(app)
     loadBattleGraphics(app)
     # initializes variables used during a battle
     app.battleBoxMsg = []
@@ -123,6 +67,18 @@ def onAppStart(app):
     backgroundColor = (0, 255, 255)  # cyan
     app.image = Image.new('RGB', (app.cellWidth, app.cellHeight), backgroundColor)
 
+def getPlayerSprites():
+    npcOverworldSpriteSheet = openImage('images/npcOverworldSprites.png')
+    playerFront = npcOverworldSpriteSheet.crop((109, 9, 14 + 109, 21 + 9))
+    playerBack = npcOverworldSpriteSheet.crop((124, 9, 14 + 124, 21 + 9))
+    playerSide = npcOverworldSpriteSheet.crop((139, 9, 13 + 139, 21 + 9))
+    playerFrontWalking = npcOverworldSpriteSheet.crop((154, 9, 15 + 154, 21 + 9))
+    playerBackWalking = npcOverworldSpriteSheet.crop((170, 9, 14 + 170, 21 + 9))
+    playerSideWalking1 = npcOverworldSpriteSheet.crop((185, 10, 14 + 185, 20 + 10))
+    playerSideWalking2 = npcOverworldSpriteSheet.crop((200, 10, 14 + 200, 20 + 10))
+    return [playerFront, playerBack, playerSide, playerFrontWalking, playerBackWalking,
+            playerSideWalking1, playerSideWalking2]
+
 def loadMapGrid():
     with open('Pokemon112/mapGrid.txt', encoding='utf-8') as f:
         gridRows = f.readlines()
@@ -133,13 +89,26 @@ def loadMapGrid():
             row = row.rstrip('\n')
             for col in row.split(','):
                 mapGrid[i].append(int(col))
+        mapGrid = np.array(mapGrid)
         return mapGrid
+
+def loadOverworldGraphics(app):
+    tilesetSheet = openImage('images/tileset.png')
+    blankTile = tilesetSheet.crop((6, 64, 16 + 6, 16 + 64))
+    grassTile = tilesetSheet.crop((6, 81, 16 + 6, 16 + 81))
+
+    app.overworldSprites = {'blankTile': blankTile, 'grassTile': grassTile}
+
+def loadMenuGraphics(app):
+    bagSpritesSheet = openImage('images/bagScreen.png')
+    itemScreen = bagSpritesSheet.crop((112, 384, 240 + 112, 160 + 384))
+    itemLabel = bagSpritesSheet.crop((16, 384, 79 + 16, 30 + 384))
+
+    app.bagSprites = {'itemScreen': itemScreen, 'itemLabel': itemLabel}
 
 def loadBattleGraphics(app):
     grassBackground = openImage('images/battleGrassBackground.png')
     app.battleBackgrounds = [grassBackground]
-    battleBoxBackground = openImage('images/battleBoxBackground.png')
-    app.battleSprites = [battleBoxBackground]
 
     battleSceneSpriteSheet = openImage('images/battleSceneSprites.png')
     oppHealthBox = battleSceneSpriteSheet.crop((3, 3, 3 + 100, 3 + 29))       # (left, top, left+width, top+height)
@@ -211,25 +180,43 @@ def setupOverworld(app):
     for row in range(rows):
         for col in range(cols):
             if app.mapGrid[row][col] == 0:      # blank
-                color = (255, 255, 255)         # white
+                cell = app.overworldSprites['blankTile']
             elif app.mapGrid[row][col] == 1:    # tree
                 color = (25, 100, 40)           # dark green
+                cell = Image.new('RGB', (app.cellWidth, app.cellHeight), color)
             elif app.mapGrid[row][col] == 2:    # barrier
                 color = (115, 70, 30)           # brown
+                cell = Image.new('RGB', (app.cellWidth, app.cellHeight), color)
             elif app.mapGrid[row][col] == 3:    # grass
-                color = (70, 180, 70)           # light green
-            cell = Image.new('RGB', (app.cellWidth, app.cellHeight), color)
+                cell = app.overworldSprites['grassTile']
             overworld[row][col] = cell
     return overworld
 
-def setupPokemonBattle(app, wild=True):
+def setupPokemonBattle(app, trainer=None):
     app.scene = 1
     app.currentAction = ''
     app.actionIndex = [0, 0]
-    if wild:
-        app.oppPokemon = Pokemon('Pidgey', 2)
+    if app.wildBattle:
+        randomPokemonNum = random.randint(1, 100)
+        if 0 < randomPokemonNum <= 30:
+            randomPokemon = 'Caterpie'
+        elif 30 < randomPokemonNum <= 50:
+            randomPokemon = 'Weedle'
+        elif 50 < randomPokemonNum <= 70:
+            randomPokemon = 'Oddish'
+        elif 70 < randomPokemonNum <= 85:
+            randomPokemon = 'Rattata'
+        elif 85 < randomPokemonNum <= 94:
+            randomPokemon = 'Pidgey'
+        elif 94 < randomPokemonNum <= 98:
+            randomPokemon = 'Spearow'
+        else:
+            randomPokemon = 'Pikachu'
+        app.oppPokemon = Pokemon(randomPokemon, random.randint(2, 4))
         app.oppPokemon.updateStats()
         app.oppPokemon.updateMoves()
+    else:
+        app.oppPokemon = trainer.party[0]
     app.battleBoxMsg = [f'What will', f'{app.curPokemon.nickName} do?']
 
 def updateScene(app):
@@ -244,23 +231,26 @@ def updateScene(app):
             app.cellD = [32, 32]
             app.cameraPosD = [0, 0]
             app.playerPos = [app.cameraPos[0] + 5, app.cameraPos[1] + 7]
+            app.player.isMoving = False
+
             checkForBattle(app)
     if app.scene == 1:  # if the player is in a battle
         pass
 
 def checkForBattle(app):
+    if (app.playerPos[0] == app.opponent.y and app.opponent.x - 5 < app.playerPos[1] < app.opponent.x
+            and not app.opponent.defeated):
+        app.wildBattle = False
+        setupPokemonBattle(app, app.opponent)
     if app.mapGrid[app.playerPos[0]][app.playerPos[1]] == 3:
         if randint(1, 10) == 1:
-            setupPokemonBattle(app, True)
+            app.wildBattle = True
+            setupPokemonBattle(app)
 
 def redrawAll(app):
     if app.scene == 0:      # if the player is in the overworld
         drawOverworld(app)
-        # for i in range(15):
-        #     drawLine(0 + i*32, 0, 0 + i*32, app.height, opacity=50)
-        #     drawLine(0, 0 + i*32, app.width, 0 + i*32, opacity=50)
-
-        drawImage(CMUImage(app.image), app.width//2, app.height//2, align='center')
+        drawTrainer(app.player, 240, 160)
     elif app.scene == 1:    # if the player is in a battle
         drawBattleBackground(app)
         drawPokemonHealthBox(app)
@@ -269,6 +259,20 @@ def redrawAll(app):
         else:
             drawBattleBoxMsg(app)
             drawActionBox(app)
+    elif app.scene == 2:
+        drawMenu(app)
+
+def drawMenu(app):
+    if app.menuScreens[app.menuScreenIndex] == 'item':
+        drawImage(CMUImage(app.bagSprites['itemScreen']), 0, 0, width=480, height=320)
+        drawImage(CMUImage(app.bagSprites['itemLabel']), 0, 0, width=158, height=60)
+
+        startLeft = 196
+        startTop = 28
+        for item in app.player.items:
+            drawAlphaNum(app, startLeft, startTop, "%-25s x %+3s" % (item, app.player.items[item]))
+            startTop += 36
+        drawAlphaNum(app, startLeft, startTop, 'CANCEL')
 
 def drawOverworld(app):
     rows, cols = 13, 17
@@ -280,19 +284,26 @@ def drawOverworld(app):
         if col != 0 and col % cols == 0:
             col = 0
             row += 1
-        drawImage(CMUImage(app.overworld[startRow+row][startCol+col]), col*32-app.cellD[1], row*32-app.cellD[0]-16)
+        drawImage(CMUImage(app.overworld[startRow+row][startCol+col]), col*32-app.cellD[1], row*32-app.cellD[0]-16,
+                  width=33, height=33)
+        if app.opponent.x == startCol + col and app.opponent.y == startRow + row:
+            drawTrainer(app.opponent, col*32-app.cellD[1], row*32-app.cellD[0]-16)
         col += 1
 
     # for row in range(rows):
     #     for col in range(cols):
     #         drawImage(CMUImage(app.overworld[startRow+row][startCol+col]), col*32-app.cellD[1], row*32-app.cellD[0])
 
+def drawTrainer(trainer, x, y):
+    # sprite variables that are currently unused (graphics will be added on later)
+    drawImage(CMUImage(trainer.getSprite()), x, y, width=28, height=40, align='center')
+
 def drawBattleBackground(app):
     drawImage(CMUImage(app.battleBackgrounds[0]), 0, 0, width=480, height=224)
     drawImage(CMUImage(app.battleSceneSprites['battleBoxBackground']), 0, 224, width=480, height=96)
 
 def drawPokemonHealthBox(app):
-    # temporary labels for opponent and player pokemon
+    # opponent pokemon
     drawImage(CMUImage(app.battleSceneSprites['oppHealthBox']), 28, 34, width=200, height=58)
     left = 107
     top = 68
@@ -303,7 +314,7 @@ def drawPokemonHealthBox(app):
     left = 190
     top = 42
     drawAlphaNum(app, left, top, str(app.oppPokemon.level))
-
+    # player pokemon
     drawImage(CMUImage(app.battleSceneSprites['playerHealthBox']), 254, 150, width=208, height=74)
     left = 350
     top = 184
@@ -317,6 +328,9 @@ def drawPokemonHealthBox(app):
     left = 384
     top = 196
     drawAlphaNum(app, left, top, f'{app.curPokemon.currentHP}/ {app.curPokemon.hp}')
+    left = 318
+    top = 216
+    drawExpBar(app, left, top, app.curPokemon)
 
 def drawHealthBar(app, left, top, currentHP, hp):
     healthPercent = currentHP / hp
@@ -331,6 +345,16 @@ def drawHealthBar(app, left, top, currentHP, hp):
 
     if healthWidth > 0:
         drawImage(CMUImage(app.battleSceneSprites[healthColor]), left, top, width=healthWidth*2, height=6)
+
+def drawExpBar(app, left, top, pokemon):
+    currentExp = pokemon.experience - pokemon.level ** 3
+    maxExp = (pokemon.level + 1) ** 3
+    expPercent = currentExp / maxExp
+    expBarWidth = 128
+    expWidth = int(expBarWidth * expPercent)
+
+    if expWidth > 0:
+        drawImage(CMUImage(app.battleSceneSprites['expBar']), left, top, width=expWidth, height=4)
 
 def drawBattleBoxMsg(app):
     startLeft = 22
@@ -375,6 +399,54 @@ def drawActionBox(app):
     cursorX, cursorY = app.actionCursorPos[app.actionIndex[1]][app.actionIndex[0]]
     drawImage(CMUImage(app.battleSceneSprites['blackCursor']), cursorX, cursorY, width=12, height=20)
 
+def advanceBattleTurn(app, playerMove, opponentMove):
+    if app.curPokemon.speed > app.oppPokemon.speed:
+        playerTurn(app, playerMove)
+
+        if app.oppPokemon.currentHP == 0:
+            if not app.wildBattle:
+                app.opponent.defeated = True
+            app.scene = 0
+            app.curPokemon.gainExperience(app.oppPokemon)
+            return
+
+        opponentTurn(app, opponentMove)
+
+        if app.curPokemon.currentHP == 0:
+            app.scene = 0
+            app.stop()
+            return
+    else:
+        opponentTurn(app, opponentMove)
+
+        if app.curPokemon.currentHP == 0:
+            app.scene = 0
+            app.stop()
+            return
+
+        playerTurn(app, playerMove)
+
+        if app.oppPokemon.currentHP == 0:
+            if not app.wildBattle:
+                app.opponent.defeated = True
+            app.scene = 0
+            app.curPokemon.gainExperience(app.oppPokemon)
+            return
+
+    app.currentAction = ''
+
+def playerTurn(app, playerMove):
+    if playerMove == 'attack':
+        moveI = app.moveIndex[0] * 2 ** 0 + app.moveIndex[1] * 2 ** 1
+        move = app.curPokemon.moves[moveI]
+        app.curPokemon.attackPokemon(app.oppPokemon, move)
+    elif playerMove == 'item':
+        pass
+
+def opponentTurn(app, opponentMove):
+    if opponentMove == 'attack':
+        app.oppPokemon.attackPokemon(app.curPokemon, app.oppPokemon.moves[0])
+
 def playerCanMove(app, move):
     playerPos = [app.cameraPos[0] + 5, app.cameraPos[1] + 7]
     if app.mapGrid[playerPos[0] + move[0]][playerPos[1] + move[1]] not in (1, 2):
@@ -382,18 +454,31 @@ def playerCanMove(app, move):
     return False
 
 def onKeyHold(app, keys):
-    if app.counter % 8 == 0:
+    if app.counter % 4 == 0:
         if app.scene == 0:
             if keys[0] == 'up' and app.cameraPosD[1] == 0 and playerCanMove(app, [-1, 0]):
-                app.cameraPosD[0] = -4
+                app.cameraPosD[0] = -8
+                app.player.isMoving = True
             if keys[0] == 'down' and app.cameraPosD[1] == 0 and playerCanMove(app, [1, 0]):
-                app.cameraPosD[0] = 4
+                app.cameraPosD[0] = 8
+                app.player.isMoving = True
             if keys[0] == 'left' and app.cameraPosD[0] == 0 and playerCanMove(app, [0, -1]):
-                app.cameraPosD[1] = -4
+                app.cameraPosD[1] = -8
+                app.player.isMoving = True
             if keys[0] == 'right' and app.cameraPosD[0] == 0 and playerCanMove(app, [0, 1]):
-                app.cameraPosD[1] = 4
+                app.cameraPosD[1] = 8
+                app.player.isMoving = True
 
 def onKeyPress(app, key):
+    if app.scene == 0:
+        if key == 'up':
+            app.player.facing = 'back'
+        if key == 'down':
+            app.player.facing = 'front'
+        if key == 'left':
+            app.player.facing = 'left'
+        if key == 'right':
+            app.player.facing = 'right'
     if app.scene == 1:
         if app.currentAction == '':
             if key == 'left' or key == 'right':
@@ -403,7 +488,10 @@ def onKeyPress(app, key):
             if key == 'x':
                 if app.actionIndex == [0, 0]:
                     app.currentAction = 'fight'
-                if app.actionIndex == [1, 1]:
+                elif app.actionIndex == [1, 0]:
+                    app.currentAction = 'bag'
+                    app.scene = 2
+                elif app.actionIndex == [1, 1] and app.wildBattle:
                     app.currentAction = 'run'
                     app.scene = 0
         elif app.currentAction == 'fight':
@@ -415,19 +503,22 @@ def onKeyPress(app, key):
                 moveI = app.moveIndex[0]*2**0 + app.moveIndex[1]*2**1
                 if moveI >= len(app.curPokemon.moves):
                     return
+                if app.curPokemon.currentMovePP[moveI] == 0:
+                    return
 
-                move = app.curPokemon.moves[moveI]
-                app.curPokemon.attackPokemon(app.oppPokemon, move)
+                advanceBattleTurn(app, 'attack', 'attack')
+            if key == 'z':
                 app.currentAction = ''
-
-                if app.oppPokemon.currentHP == 0:
-                    app.scene = 0
-                    return
-                app.oppPokemon.attackPokemon(app.curPokemon, app.oppPokemon.moves[0])
-                if app.curPokemon.currentHP == 0:
-                    app.scene = 0
-                    app.stop()
-                    return
+    elif app.scene == 2:
+        if app.currentAction == 'bag':
+            if key == 'x':
+                if app.player.items:
+                    app.player.useItem('Potion', app.player.party[0])
+                    app.scene = 1
+                    advanceBattleTurn(app, 'item', 'attack')
+            if key == 'z':
+                app.currentAction = ''
+                app.scene = 1
 
 def onStep(app):
     app.counter += 1
